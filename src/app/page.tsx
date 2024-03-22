a36d4faa90295de548c61de8957b86fdf65d4430
PIVIIII <lenvariya@gmail.com>
2024-03-22 22:48:29 +0700

    eieieieiei

---
+++ src/app/page.tsx
@@ -0,0 +1,14 @@
+import Image from "next/image";
+import Banner from "@/components/Banner";
+import styles from "./page.module.css";
+import ProductCard from '@/components/ProductCard'
+import CarPanel from "@/components/CarPanel";
+import { TravelCard } from "@/components/TravelCard";
+export default function Home() {
+  return (
+    <main>
+      <Banner/>
+      <TravelCard/>
+    </main>
+  );
+}
