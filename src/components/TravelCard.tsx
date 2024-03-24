9b4a6f408353f50d0bc26514042c807382c43e03
PIVIIII <lenvariya@gmail.com>
2024-03-25 02:45:49 +0700

    edit uxui (not final) video

--- src/components/TravelCard.tsx
+++ src/components/TravelCard.tsx
@@ -8,25 +8,21 @@ export function TravelCard() {
 
     const [playing,setPlaying] = useState(true)
     const [rating,setRating] = useState(0)
-    const [pointerPosition,setPointerPosition] = useState({x:0,y:0})
-
-    useWindowListener('pointermove', (e)=> {
-        setPointerPosition( { x:(e as PointerEvent).clientX, y:(e as PointerEvent).clientY })
-    })
 
     return (
-        <div className="w-[80%] shadow-lg mx-[10%] my-10 p-2 rounded-lg bg-gray-200 flex flex-row">
+        <div className="w-[100%] mt-20 rounded-lg bg-white flex flex-col justify-center items-center">
             <VlogPlayer vdoSrc="/video/ThailandNatural.mp4" isPlaying={playing}></VlogPlayer>
-            <div className="m-5">Thailand Natural ( {pointerPosition.x},{pointerPosition.y} )
+            <div className="m-5">
                 <button className="block rounded-md bg-sky-600 hover:bg-indigo-600 px-3 py-2
                 text-white shadow-sm"
                 onClick={ ()=> { setPlaying(!playing) } }>
                 { playing? 'Pause':'Play'}
                 </button>
-                <Rating className="w-full h-[10%]" value={ (rating==undefined)? 0:rating }
-                onChange={ (e,newValue)=>{if(newValue!=null) setRating(newValue)} } />
             </div>
             
         </div>
     )
 }
+
+{/* <Rating className="w-full h-[10%]" value={ (rating==undefined)? 0:rating }
+onChange={ (e,newValue)=>{if(newValue!=null) setRating(newValue)} } /> */}
--- src/components/VlogPlayer.tsx
+++ src/components/VlogPlayer.tsx
@@ -7,13 +7,10 @@ export function VlogPlayer( {vdoSrc,isPlaying}:{vdoSrc:string,isPlaying:boolean}
     const vdoRef = useRef<HTMLVideoElement>(null)
     
     useEffect( ()=> {
-        //alert('width is '+ vdoRef.current?.videoWidth)
         if(isPlaying) {
-            //alert('Play VDO')
             vdoRef.current?.play()
         }
         else {
-            //alert('Pause VDO')
             vdoRef.current?.pause()
         }
     },[isPlaying])
