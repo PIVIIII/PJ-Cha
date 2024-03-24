9ea098f6b769d00525cb764c37cf3bd808974e74
PIVIIII <lenvariya@gmail.com>
2024-03-24 11:34:50 +0700

    delete css topmenu

--- src/components/TopMenu.tsx
+++ src/components/TopMenu.tsx
@@ -9,8 +9,9 @@ export default async function Topmenu() {
     const session = await getServerSession(authOptions)
 
     return (
-        <div className={styles.menucontainer}> 
-            <Image src={'/img/logo.png'} className={styles.logoimg} alt='logo'
+            <div className="h-16 bg-blue-900 fixed top-0 left-0 right-0 
+            z-30 border-t border-b border-gray-200 flex flex-row text-white">
+            <Image src={'/img/logo.png'} className='h-full w-auto' alt='logo'
             width={0} height={0} sizes='100vh'/>
             <TopMenuItem title='Select Car' pageRef='/car'/>
             <TopMenuItem title='Reservations' pageRef='/reservations'/>
@@ -20,10 +21,10 @@ export default async function Topmenu() {
             <TopMenuItem title='Cart' pageRef='/cart'/>
             {
                 session? <Link href="api/auth/signout">
-                    <div className='flex items-center h-full px-2 text-cyan-600 text-sm'>Sign-Out of {session.user?.name}</div>
+                    <div className='flex items-center h-full px-2 text-white text-sm'>Sign-Out of {session.user?.name}</div>
                 </Link>
                 :  <Link href="api/auth/signin">
-                    <div className='flex items-center h-full px-2 text-cyan-600 text-sm'>
+                    <div className='flex items-center h-full px-2 text-white text-sm'>
                     Sign-In</div>
                 </Link>
             } 
--- src/components/TopMenuItem.tsx
+++ src/components/TopMenuItem.tsx
@@ -1,11 +1,10 @@
 import Link from "next/link";
-import styles from './topmenu.module.css'
 
 export default function TopMenuItem( { title , pageRef } : { title : string , pageRef : string }  ) {
     return (
-        <Link href={pageRef} className={styles.itemcontainer}>
+        <Link href={pageRef} className='w-120 mx-5 flex items-center h-full px-2 
+        font-sans text-sm text-white'>
             {title}
         </Link>
-
     );
 }
