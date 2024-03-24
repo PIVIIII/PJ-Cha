3de12126fabfa666e8ff36deefa951943057091f
PIVIIII <lenvariya@gmail.com>
2024-03-24 19:55:25 +0700

    change color Topmenu + edit uxui (not final) /car/cid

--- src/app/(carinfo)/car/[cid]/page.tsx
+++ src/app/(carinfo)/car/[cid]/page.tsx
@@ -5,37 +5,37 @@ import Link from "next/link"
 export default async function CarDetailPage( {params} : { params : {cid:string} } ) {
     const carDetail = await getCar(params.cid)
 
-    /*const mockCarRepo = new Map()
-    mockCarRepo.set("001", {name:"Honda Civic",image:"/img/civic.jpg"})
-    mockCarRepo.set("002", {name:"Honda Accord",image:"/img/accord.jpg"})
-    mockCarRepo.set("003", {name:"Toyota Forture",image:"/img/fortuner.jpg"})
-    mockCarRepo.set("004", {name:"Tesla Model 3",image:"/img/tesla.jpg"})*/
-    
-    
     return (
-        <main className="text-center p-5">
-            <h1 className="text-lg font-medium">{carDetail.data.model}</h1>
-            <div className="flex flex-row my-5">
-                <Image src={ carDetail.data.picture }
+        <main className="text-center p-2">
+            <h1 className="text-lg mt-10 text-3xl font-serif  ">{carDetail.data.model}</h1>
+            <div className="mx-auto my-5 w-[30%] p-2 rounded-lg bg-white flex flex-col">
+                <div className="relative w-full h-60">
+                    <Image
+                    src={carDetail.data.picture}
                     alt='Car Image'
-                    width={0} height={0} sizes="100vw"
-                    className="rounded-lg w-[30%]"/>
-                <div className="text-md mx-5 text-left">{ carDetail.data.description }
-                <div className="text-md mx-5">Doors: { carDetail.data.doors }</div>
-                <div className="text-md mx-5">Seats: { carDetail.data.seats }</div>
-                <div className="text-md mx-5">Large Bags: { carDetail.data.largebags }</div>
-                <div className="text-md mx-5">Small Bags{ carDetail.data.smallbags }</div>
-                <div className="text-md mx-5">Daily Rental Rate: { carDetail.data.dayRate }(insurance included)</div>
+                    layout='fill'
+                    objectFit='cover'
+                    />
+                </div>
+                <div className="bg-yellow-300 text-lg text-center font-serif py-2"> Description </div>   
+                <div className="text-md bg-yellow-100 font-mono py-3 ">{ carDetail.data.description }
+                        <div className="text-md ">Doors: { carDetail.data.doors }</div>
+                        <div className="text-md ">Seats: { carDetail.data.seats }</div>
+                        <div className="text-md ">Large Bags: { carDetail.data.largebags }</div>
+                        <div className="text-md ">Small Bags{ carDetail.data.smallbags }</div>
+                        <div className="text-md ">Daily Rental Rate: { carDetail.data.dayRate }</div>
+                </div>          
+            </div>
                 
+            <div className='flex items-center justify-center h-full'>
                 <Link href={`/reservations?id=${params.cid}&model=${carDetail.data.model}`}>
-                    <button className="block rounded-md bg-sky-600 hover:bg-indigo-600 px-3 py-2
+                    <button className="block rounded-md bg-rose-300 hover:bg-indigo-200 px-3 py-2
                     shadow-sm text-white">
                         Make Reservation
                     </button>
                 </Link>
-                
-                </div>                    
             </div>
+
         </main>
     )
 }
--- src/components/TopMenu.tsx
+++ src/components/TopMenu.tsx
@@ -9,7 +9,7 @@ export default async function Topmenu() {
     const session = await getServerSession(authOptions)
 
     return (
-            <div className="h-16 bg-blue-900 fixed top-0 left-0 right-0 
+            <div className="h-16 bg-indigo-500 fixed top-0 left-0 right-0 
             z-30 border-t border-b border-gray-200 flex flex-row text-white">
             <Image src={'/img/logo.png'} className='h-full w-auto' alt='logo'
             width={0} height={0} sizes='100vh'/>
