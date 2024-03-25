0987c9b34bf97008130025371e393c1778e90cce
PIVIIII <lenvariya@gmail.com>
2024-03-25 16:00:52 +0700

    edit uxui-2 AboutUs

--- src/components/Aboutus.tsx
+++ src/components/Aboutus.tsx
@@ -10,25 +10,26 @@ export default function AboutUs() {
         <div className="mt-20 rounded-lg bg-white flex flex-col justify-center items-center">
             <div className="text-2xl font-mono">ABOUT US</div>
             <div className="mt-5 text-lg font-mono text-center">
-                <div>We are committed to providing you with a memorable experience every time you choose to rent a car with us.</div>
-                <div>We not only offer car rental services but also provide convenience and assurance that you will have an excellent experience every time.</div>
-                <div>With quality vehicles and professional service, we are ready to make your journey enjoyable and truly impressive.</div>
+                <div>We're dedicated to making every car rental experience with us memorable.</div>
+                <div>Beyond just providing cars, we ensure convenience and assurance</div>
+                <div>for an excellent experience each time. With top-notch vehicles and professional service,</div>
+                <div>we're here to make your journey truly enjoyable and impressive</div>
             </div>
             <div className="flex flex-row mt-5">
                 <div className='rounded-lg w-[230px] h-[350px] mx-3 my-5 relative '>
                     <Image src={'/img/au1.jpg'} alt='Product Picture'
                         fill={true} className='object-cover absolute rounded-lg shadow-xl'/>
-                    <div className="relative top-20 z-20 text-center text-2xl font-medium">Discounts & benefit</div>
+                    <div className="relative top-10 z-20 text-center text-2xl font-medium">Discounts <div> & benefit</div></div>
                 </div>
                 <div className='rounded-lg w-[230px] h-[350px] mx-3 my-5 relative '>
                     <Image src={'/img/au2.jpg'} alt='Product Picture'
                         fill={true} className='object-cover absolute rounded-lg shadow-xl'/>
-                    <div className="relative top-20 z-20 text-center text-2xl font-medium">Daily, weekly or monthly</div>
+                    <div className="relative top-10 z-20 text-center text-2xl font-medium">Daily, weekly <div>or monthly</div></div>
                 </div>
                 <div className='rounded-lg w-[230px] h-[350px] mx-3 my-5 relative '>
                     <Image src={'/img/au3.jpg'} alt='Product Picture'
                         fill={true} className='object-cover absolute rounded-lg shadow-xl'/>
-                    <div className="relative top-20 z-20 text-center text-2xl font-medium">Online check-in</div>
+                    <div className="relative top-10 z-20 text-center text-2xl font-medium">Online <div>check-in</div></div>
                 </div>
             </div>
         </div>
